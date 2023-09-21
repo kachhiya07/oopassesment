8354250ab874e1964017932bcd86284aa6772c23
kachhiya07 <134249876+kachhiya07@users.noreply.github.com>
2023-09-21 10:48:17 +0530

    Update program1.cpp

--- program1.cpp
+++ program1.cpp
@@ -1,10 +1,42 @@
 #include<iostream>
 using namespace std;
-int main()
-{
-	
-	
-	
-	
+class teacher {
+
+		int no_of_lectures,i;
+		string lecturer,subject,course,lectures;
+
+	public:
+		teacher() {
+			cout<<"what is name of lecturer ? :";
+			cin>>lecturer;
+			cout<<"enter subject:";
+			cin>>subject;
+			cout<<"name of course ? :";
+			cin>>course;
+
+		}
+		void add_details() {
+			cout<<"enter 5 lectures:"<<endl;
+			for(i=1; i<=5; i++) {
+				cin>>lectures;
+			}
+		}
+
+		void display() {
+			cout<<"lecturer name is :"<<lecturer<<endl;
+			for(i=1; i<=5; i++) {
+				cout<<"lecture is "<<lectures<<endl;
+			}
+		}
+
+};
+int main() {
+	teacher t;
+	t.add_details();
+	t.display();
+
+
+
+
 	return 0;
 }
