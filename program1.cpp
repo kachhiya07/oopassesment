76a03079c54436710cd206c5ab1412813c11da17
kachhiya07 <134249876+kachhiya07@users.noreply.github.com>
2023-09-25 13:20:19 +0530

    Update program1.cpp

--- program1.cpp
+++ program1.cpp
@@ -1,42 +1,50 @@
+//Data members:
+//a) Name of the lecturer
+//b) Name of the subject
+//c) Name of course
+//d) Number of lecturers
+
+
+//Data functions:
+//a) To assign initial values
+//b) To add a lecture details
+//c) To display name and lecture details 
+
 #include<iostream>
 using namespace std;
-class teacher {
-
-		int no_of_lectures,i;
-		string lecturer,subject,course,lectures;
-
+class lecture{
 	public:
-		teacher() {
-			cout<<"what is name of lecturer ? :";
-			cin>>lecturer;
-			cout<<"enter subject:";
-			cin>>subject;
-			cout<<"name of course ? :";
-			cin>>course;
-
-		}
-		void add_details() {
-			cout<<"enter 5 lectures:"<<endl;
-			for(i=1; i<=5; i++) {
-				cin>>lectures;
-			}
-		}
-
-		void display() {
-			cout<<"lecturer name is :"<<lecturer<<endl;
-			for(i=1; i<=5; i++) {
-				cout<<"lecture is "<<lectures<<endl;
-			}
-		}
-
+	
+		string name,subject,course;
+		
 };
-int main() {
-	teacher t;
-	t.add_details();
-	t.display();
+int main()
+{
+	int i;
+	lecture obj[5];
+	for(i=0;i<5;i++)
+	{
+		cout<<"lecturer no : "<<i+1<<endl;
+		cout<<"enter lecturer name : ";
+		cin>>obj[i].name;
+		cout<<"enter subject name:";
+		cin>>obj[i].subject;
+		cout<<"enter course name:";
+		cin>>obj[i].course;
+		cout<<endl;
+	}
+	cout<<"-----display of lecturer------"<<endl;
+	for(i=0;i<5;i++)
+	{
+	cout<<"lecturer:"<<i+1<<endl;
+	cout<<" lecturer name : ";
+	cout<<obj[i].name<<endl;
+	cout<<" subject name : ";
+	cout<<obj[i].subject<<endl;
+	cout<<"course name : ";
+	cout<<obj[i].course<<endl;
+	cout<<endl;
+	}
 
-
-
-
-	return 0;
+	
 }
